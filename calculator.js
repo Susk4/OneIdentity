236a7aa718532178af021d4cdea83fd0535d3d06
Susk4 <35198313+Susk4@users.noreply.github.com>
2023-06-16 08:24:23 +0200

    validations

--- calculator.js
+++ calculator.js
@@ -1,15 +1,55 @@
 const readline = require('readline');
 
-// Function to parse and convert the fraction string to a decimal number
-function parseFraction(fraction) {
-  if (fraction.includes('&')) {
-    const [whole, fr] = fraction.split('&');
-    const [numerator, denominator] = fr.split('/');
-    return parseInt(whole, 10) + parseInt(numerator, 10) / parseInt(denominator, 10);
-  } else {
-    const [numerator, denominator] = fraction.split('/');
-    return parseInt(numerator, 10) / parseInt(denominator, 10);
+// function parseFraction(fraction) {
+function validateString(input) {
+  const regex = /^[0-9&/]+$/;
+  return regex.test(input);
+}
+// Function to validate the operand string
+function validateOperand(operand) {
+  if (!validateString(operand)) {
+    throw new Error('Operand should be a number,fraction or improper fraction');
   }
+
+  if (operand.includes('&')) {
+    const [_, fraction] = operand.split('&');
+    if (fraction.includes('/')) {
+      validateFraction(fraction);
+    }
+    else {
+      throw new Error('Fraction should be in the format of numerator/deniminator');
+    }
+  } else {
+    if (operand.includes('/')) {
+      validateFraction(operand);
+    }
+  }
+}
+// Function to validate the fraction string
+function validateFraction(fraction) {
+  const [numerator, deniminator] = fraction.split('/');
+  if (numerator > deniminator) {
+    throw new Error('Numerator should be less than deniminator');
+  }
+}
+
+// Function to parse and convert the operand string
+function parseOperand(operand) {
+  validateOperand(operand);
+  if (operand.includes('&')) {
+    const [whole, fraction] = operand.split('&');
+    return parseInt(whole) + parseFraction(fraction);
+  } else {
+    return parseFraction(operand);
+  }
+}
+
+// Function to parse and convert the fraction string
+function parseFraction(fraction) {
+  const [numerator, deniminator] = fraction.split('/');
+  console.log(numerator, deniminator);
+  console.log(parseInt(numerator) / parseInt(deniminator))
+  return parseInt(numerator) / parseInt(deniminator);
 }
 
 // Function to convert a decimal number to a fraction string
@@ -53,7 +93,7 @@ function performOperation(operator, operand1, operand2) {
       result = operand1 / operand2;
       break;
     default:
-      throw new Error('Invalid operator');
+      throw new Error(`'${operator}' is not a valid operator. Valid operators are +, -, *, and /.`);
   }
   return result;
 }
@@ -74,10 +114,18 @@ function runCalculator() {
       return;
     }
 
-    const [operand1, operator, operand2] = input.split(' ');
     try {
-      const decimal1 = parseFraction(operand1);
-      const decimal2 = parseFraction(operand2);
+      const split = input.split(' ').filter((item) => item !== '');
+
+      if (!input.includes(' ')) {
+        throw new Error('Operands and operator must be separated by spaces.');
+      }
+      else if (split.length !== 3) {
+        throw new Error('Only binary operations are supported.');
+      }
+      const [operand1, operator, operand2] = split;
+      const decimal1 = parseOperand(operand1);
+      const decimal2 = parseOperand(operand2);
 
       const result = performOperation(operator, decimal1, decimal2);
       const fractionResult = convertToFraction(result);
